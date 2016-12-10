67290cd8fedd927e2a42440edd183ddd8056008b
Snakevil Zen <zsnakevil@gmail.com>
2016-12-10 16:12:07 +0800

    修正日期时间组件错误处理 MySQL 默认时间 0000-00-00 00:00:00 地问题。

--- lib/Type/DateTime.php
+++ lib/Type/DateTime.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * 定义可做类型转换的日期时间组件。
+ *
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2016 SZen.in
+ * @license   LGPL-3.0+
  */
 
 namespace Zen\Core\Type;
@@ -8,25 +12,17 @@ namespace Zen\Core\Type;
 use DateTime as PHPDateTime;
 use DateTimeZone;
 
-/**
- * 可做类型转换的日期时间组件。
- *
- * @package    Zen\Core
- * @subpackage Type
- * @version    0.1.0
- * @since      0.1.0
- */
 class DateTime extends PHPDateTime
 {
     /**
-     * 构造函数
+     * 构造函数.
      *
      * @param string|int   $time 可选。时间描述
      * @param DateTimeZone $zone 可选。时区
      */
     public function __construct($time = 'now', DateTimeZone $zone = null)
     {
-        if ('epoch' == $time || '0' == $time) {
+        if ('epoch' == $time || '0' == $time || '0000-00-00 00:00:00' == $time) {
             $time = '1970-01-01 00:00:00';
         }
         if (is_numeric($time) && 2038 < $time) {
